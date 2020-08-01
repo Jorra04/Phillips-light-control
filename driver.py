216a4a3f524783b4bbaef19793bb4538b7af228c
unknown <jorra04@my.yorku.ca>
2020-07-31 21:16:53 -0400

    Added better voice control, but cannot get it to stop the program.

--- driver.py
+++ driver.py
@@ -1,43 +1,49 @@
 from controller import Phillips_Hue_Automation
 import speech_recognition as sr
-
-r = sr.Recognizer()
+import sys
+import threading
 
 
 controller = Phillips_Hue_Automation()
 
-escape = False
-while(not escape):
-    text = ''
+def callback(recognizer, audio):
+    text = ''                          # this is called from the background thread
     try:
-        with sr.Microphone() as source:
+        text = recognizer.recognize_google(audio)
+        print(text)
+        if(text == 'terminate' or text == 'Terminate'):
+            controller.turn_lamps_off()
             
-            audio_data = r.record(source,duration=1)
-            text = r.recognize_google(audio_data)
-            print(text)
+        if(text == 'Down' or text == 'down'):
+            controller.decrease_hue()
+        elif(text == 'Up' or text == 'up'):
+            controller.increase_hue()
+        elif(text == 'Left' or text == 'left'):
+            controller.decrease_sat()
+        elif(text == 'Right' or text == 'right'):
+            controller.increase_sat()
+        elif(text == 'Reset' or text == 'reset'):
+            controller.reset_vals()
+        elif(text == 'Red' or text == 'red'):
+            controller.make_colour(0,250)
+        elif(text == 'Green' or text == 'green'):
+            controller.make_colour(500,120)
+        elif(text == 'Purple' or text == 'purple'):
+            controller.make_colour(58000,250)
+        elif(text == 'On' or text == 'on'):
+            controller.turn_lamps_on()
+        elif(text == 'Off' or text == 'off'):
+            controller.turn_lamps_off()
     except:
+        # print("error")
         pass
-    
-    if(text == 'terminate' or text == 'Terminate'):
-        controller.turn_lamps_off()
-        break
-    if(text == 'Down' or text == 'down'):
-        controller.decrease_hue()
-    elif(text == 'Up' or text == 'up'):
-        controller.increase_hue()
-    elif(text == 'Left' or text == 'left'):
-        controller.decrease_sat()
-    elif(text == 'Right' or text == 'right'):
-        controller.increase_sat()
-    elif(text == 'Reset' or text == 'reset'):
-        controller.reset_vals()
-    elif(text == 'Red' or text == 'red'):
-        controller.make_colour(0,250)
-    elif(text == 'Green' or text == 'green'):
-        controller.make_colour(500,120)
-    elif(text == 'Purple' or text == 'purple'):
-        controller.make_colour(58000,250)
-    elif(text == 'On' or text == 'on'):
-        controller.turn_lamps_on()
-    elif(text == 'Off' or text == 'off'):
-        controller.turn_lamps_off()
+
+r = sr.Recognizer()
+r.energy_threshold = 4000
+r.pause_threshold = 0.8
+r.listen_in_background(sr.Microphone(), callback)
+print(threading.enumerate())
+
+# [<_MainThread(MainThread, started 164184)>, <Thread(Thread-1, started daemon 164240)>]
+import time
+while True: time.sleep(0.1) 
